c5b9991ed8a33fe1f5ffca4ec360abad6fea46e1
Краснобаев Алексей <rakeliit@protonmail.com>
2026-07-08 22:27:04 +0300

    First Commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * Test_SPI.c
+ *
+ * Created: 08.07.2026 22:12:26
+ * Author : rakel
+ */ 
+#define F_CPU 8000000UL
+#include "uart.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+int main(void)
+{
+    uart_Init();
+	
+    while (1) 
+    {
+		
+    }
+}
+
